94138049a63d7d4fd3ad56033b6cdb8ef2a4af40
Sardor <sardor@hanbiro.com>
2014-11-28 15:01:34 +0900

    Python debugger for url parsing analysis

--- uzbdictspider/settings.py
+++ uzbdictspider/settings.py
@@ -3,6 +3,6 @@
 BOT_NAME = 'uzbdictspider'
 
 SPIDER_MODULES = ['uzbdictspider.spiders']
-NEWSPIDER_MODULE = 'uzbdictspider.spiders'
+# NEWSPIDER_MODULE = 'uzbdictspider.spiders'
 
 USER_AGENT = 'uzbdictspider (+http://www.uzbek-dictionary.com)'
